b1cf2ffff73e9b9a65c5972de26c59e2e61d3075
niallo1000 <20082280@mail.wit.ie>
2019-10-14 20:03:16 +0100

    anko logger

--- app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -2,11 +2,14 @@ package org.wit.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class PlacemarkActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activityplacemark)
+        info("Placemark Activity started..")
     }
 }
